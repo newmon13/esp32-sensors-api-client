19bc17a6636bc28781c0a346773b8f60758ff8bd
Moher <47338928+newmon13@users.noreply.github.com>
2025-04-19 14:41:05 +0200

    Moved to localization monitored by Hot Reload for Vaadin

---
+++ src/main/java/dev/jlipka/esp32sensorsapiclient/watersensor/WaterSensorController.java
@@ -0,0 +1,19 @@
+package dev.jlipka.esp32sensorsapiclient.watersensor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WaterSensorController {
+    private final WaterSensorService waterSensorService;
+
+    public WaterSensorController(WaterSensorService waterSensorService) {
+        this.waterSensorService = waterSensorService;
+    }
+
+    @GetMapping("/water-sensor")
+    public WaterSensorDataDto getWaterLevel() {
+        return waterSensorService.getWaterLevel()
+                .join();
+    }
+}
