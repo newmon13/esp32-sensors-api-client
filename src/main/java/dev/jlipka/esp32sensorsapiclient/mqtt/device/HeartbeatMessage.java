db54eb6f1345adcc4ff43680f332397add69f886
Moher <47338928+newmon13@users.noreply.github.com>
2025-04-29 14:19:59 +0200

    Renamed and deleted unnecessary String property

--- src/main/java/dev/jlipka/esp32sensorsapiclient/mqtt/device/HeartbeatMessage.java
+++ src/main/java/dev/jlipka/esp32sensorsapiclient/mqtt/device/HeartbeatMessage.java
@@ -2,5 +2,5 @@ package dev.jlipka.esp32sensorsapiclient.mqtt.device;
 
 import java.time.Instant;
 
-public record Heartbeat(String heartbeat, Instant timestamp) {
+public record HeartbeatMessage(Instant timestamp) {
 }
