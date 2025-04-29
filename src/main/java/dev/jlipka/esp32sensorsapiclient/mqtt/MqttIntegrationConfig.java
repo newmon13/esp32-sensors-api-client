4bad7c74a213aa0c4dd3d5278b532d3282d78255
Moher <47338928+newmon13@users.noreply.github.com>
2025-04-29 14:15:01 +0200

    Added pendingChannel and its subscriber

--- src/main/java/dev/jlipka/esp32sensorsapiclient/mqtt/MqttIntegrationConfig.java
+++ src/main/java/dev/jlipka/esp32sensorsapiclient/mqtt/MqttIntegrationConfig.java
@@ -1,6 +1,9 @@
-package dev.jlipka.esp32sensorsapiclient.mqtt.discovery;
+package dev.jlipka.esp32sensorsapiclient.mqtt;
 
 import dev.jlipka.esp32sensorsapiclient.mqtt.device.EspTopic;
+import dev.jlipka.esp32sensorsapiclient.mqtt.discovery.DiscoveryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,20 +19,24 @@ import org.springframework.messaging.MessageHeaders;
 
 import java.util.Objects;
 
-import static dev.jlipka.esp32sensorsapiclient.mqtt.discovery.MessageMapper.extractEspTopic;
+import static dev.jlipka.esp32sensorsapiclient.mqtt.MessageMapper.extractEspTopic;
 
 
 @Configuration
 @EnableIntegration
 public class MqttIntegrationConfig {
 
-    private static final String[] TOPICS = new String[]{"system/discovery", "devices/+/status", "devices/+/heartbeat", "devices/+/sensor/#"};
+    private static final String[] TOPICS = new String[]{"system/discovery", "devices/+/heartbeat", "devices/+/sensors/#"};
+    private static final Logger log = LoggerFactory.getLogger(MqttIntegrationConfig.class);
 
     @Value("${mqtt.broker.url}")
     private String brokerUrl;
 
     @Autowired
     private DiscoveryService discoveryService;
+
+    @Autowired
+    private PendingMessageService pendingMessageService;
 
 
     @Bean
@@ -52,11 +59,6 @@ public class MqttIntegrationConfig {
     }
 
     @Bean
-    public MessageChannel statusChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
     public MessageChannel heartbeatChannel() {
         return new DirectChannel();
     }
@@ -70,15 +72,14 @@ public class MqttIntegrationConfig {
     public IntegrationFlow routerFlow() {
         return IntegrationFlow.from(mqttInboundAdapter())
                 .route(Message.class, message -> {
-
                     MessageHeaders headers = message.getHeaders();
                     String receivedTopic = headers.get(MqttHeaders.RECEIVED_TOPIC, String.class);
                     return extractEspTopic(Objects.requireNonNull(receivedTopic));
-
                 }, mapping -> mapping.channelMapping(EspTopic.DISCOVERY, discoveryChannel())
-                        .channelMapping(EspTopic.STATUS, statusChannel())
                         .channelMapping(EspTopic.HEARTBEAT, heartbeatChannel())
-                        .channelMapping(EspTopic.SENSOR, sensorChannel()))
+                        .channelMapping(EspTopic.SENSOR, sensorChannel())
+                        .defaultOutputChannel(pendingChannel())
+                        .resolutionRequired(false))
                 .get();
     }
 
@@ -89,42 +90,29 @@ public class MqttIntegrationConfig {
                 .get();
     }
 
-//
-//    @Bean
-//    public IntegrationFlow statusFlow() {
-//        return IntegrationFlow.from(statusChannel())
-//                .handle((payload, headers) -> {
-//                    String topic = headers.get(MqttHeaders.RECEIVED_TOPIC, String.class);
-//                    String deviceId = extractDeviceIdFromTopic(Objects.requireNonNull(topic));
-//                    System.out.println("Device status " + deviceId + ": " + payload);
-//                    return null;
-//                })
-//                .get();
-//    }
-//
-//    @Bean
-//    public IntegrationFlow heartbeatFlow() {
-//        return IntegrationFlow.from(heartbeatChannel())
-//                .handle((payload, headers) -> {
-//                    String topic = headers.get(MqttHeaders.RECEIVED_TOPIC, String.class);
-//                    String deviceId = extractDeviceIdFromTopic(Objects.requireNonNull(topic));
-//                    System.out.println("Device heartbeat " + deviceId);
-//                    return null;
-//                })
-//                .get();
-//    }
-//
-//    @Bean
-//    public IntegrationFlow sensorFlow() {
-//        return IntegrationFlow.from(sensorChannel())
-//                .handle((payload, headers) -> {
-//                    String topic = headers.get(MqttHeaders.RECEIVED_TOPIC, String.class);
-//                    String deviceId = extractDeviceIdFromTopic(Objects.requireNonNull(topic));
-//                    String sensorType = extractSensorTypeFromTopic(topic);
-//                    System.out.println("Data from sensor " + sensorType + " of device " + deviceId + ": " + payload);
-//
-//                    return null;
-//                })
-//                .get();
-//    }
+    @Bean
+    public MessageChannel pendingChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public IntegrationFlow heartbeatToPendingFlow() {
+        return IntegrationFlow.from(heartbeatChannel())
+                .channel(pendingChannel())
+                .get();
+    }
+
+    @Bean
+    public IntegrationFlow sensorToPendingFlow() {
+        return IntegrationFlow.from(sensorChannel())
+                .channel(pendingChannel())
+                .get();
+    }
+
+    @Bean
+    public IntegrationFlow pendingFlow() {
+        return IntegrationFlow.from(pendingChannel())
+                .handle(pendingMessageService::add)
+                .get();
+    }
 }
