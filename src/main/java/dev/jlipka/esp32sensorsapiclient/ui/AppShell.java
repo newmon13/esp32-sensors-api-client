4ab870acdbd962d5c866d81c9425525f5cf442f3
Moher <47338928+newmon13@users.noreply.github.com>
2025-04-29 14:23:01 +0200

    Added config class that allows dynamic changes of view

---
+++ src/main/java/dev/jlipka/esp32sensorsapiclient/ui/AppShell.java
@@ -0,0 +1,8 @@
+package dev.jlipka.esp32sensorsapiclient.ui;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+
+@Push
+public class AppShell implements AppShellConfigurator {
+}
